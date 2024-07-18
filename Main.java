6937390e03a3b3e7bb8c957c5d1110f536a858e5
saichethanr <saichethan9019@gmail.com>
2024-07-18 17:28:54 +0530

    quiz wiz

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        QuestionService  q = new QuestionService();
+        q.displayquestions();
+    }
+}
---
+++ QuestionService.java
@@ -0,0 +1,14 @@
+public class QuestionService {
+ Question [] questions = new Question[5];
+ public QuestionService(){
+    questions[0] = new Question(1, "what is this lang?", "java", "cpp", "python", "c#", "java");
+    questions[1] = new Question(1, "what is ur fav lang", "java", "cpp", "python", "c#", "java");
+    questions[2] = new Question(1, "what", "java", "cpp", "python", "c#", "java");
+    questions[3] = new Question(1, "what", "java", "cpp", "python", "c#", "java");
+    questions[4] = new Question(1, "what", "java", "cpp", "python", "c#", "java");
+ }
+ public void displayquestions(){
+
+ }
+
+}
